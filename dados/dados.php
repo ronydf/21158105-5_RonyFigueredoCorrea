d05b21b86453e736f7cf567081ff009132df3e15
ronydf <correa.rony@gmail.com>
2021-08-13 08:59:37 -0300

    Criação da página dados.php

---
+++ dados/dados.php
@@ -0,0 +1,33 @@
+<?php
+	$disciplina = array(
+		'teacher' =>'Rafael A. FLorindo',
+		'photo'=>'logo.png',
+		'discipline' => 'Programação Back End I',
+		'module'=> '53/2021'
+	);
+
+	$conteudo = array(
+		0 => array('id'=>1, 'title'=> 'HTML', 'description' => "Linguagem de Marcação - Em essência trata-se de um conjunto de regras, códigos e marcadores (ou tags) utilizados para definir principalmente para definir como o conteúdo da página será apresentado."),
+		1 => array('id'=>2, 'title'=> 'CSS', 'description' =>  "Cascading Style Sheets é um mecanismo para adicionar estilo a um documento web. O código CSS pode ser aplicado diretamente nas tags ou ficar contido dentro das tags < style >. Também é possível, em vez de colocar a formatação dentro do documento, criar um link para um arquivo CSS que contém os estilos."),
+		2 => array('id'=>3,  'title'=> 'PHP', 'description' => "Linguagem de Programação - É uma linguagem interpretada e livre que atua do lado do servidor, tem seu melhor uso no desenvolvimento de aplicações web dinâmicas")
+	);
+
+	$dados = array(
+		0 => array('title' => 'Aula 01', 
+			'description' => 'Nesta aula iremos aprofundar nossos conhecimento na linguagem de marcação HTML que é de extrema importância no desenvolvimento Web. Pois ela é a responsável por marcar os conteúdos a serem renderizados em nossas páginas. Iremos utilizar o HTML em todas as nossas aulas para marcação dos conteúdos.',
+			'date' => '2020-07-27',
+			'link'=> ''),
+		1 => array('title' => 'Aula 02', 
+			'description' => 'Nesta aula iremos iniciar nossos estudos a linguagem de programação PHP, linguagem esta responsável por mais de 80% do funcionamento dos web sites da Web. Se o HTML é a responsável por marcar os conteúdos a serem renderizados em nossas página, o PHP é responsável por todas as regrasde negócios de nossa aplicação e gerenciamento dos conteúdos.',
+			'date' => '2020-08-03',
+			'link'=> ''),
+		2 => array('title' => 'Aula 03', 
+			'description' => 'Partindo da aula anterior, iremos aprofundar nossos conhecimentos nas estruturas da linguagem, ou seja, nesta aula vamos aplicar as estruturas de controle: condição e repetição',
+			'date' => '2020-08-10',
+			'link'=> ''),
+		3 => array('title' => 'Aula 04', 
+			'description' => 'Para fechar a disciplina iremos trabalhar com inclusão de arquivos e funções. Este conteúdo é de extrema importância pois permite o reuso de códigos.',
+			'date' => '2020-08-17',
+			'link'=> ''
+		)
+	);
--- index.php
+++ index.php
@@ -10,38 +10,53 @@
     <title>Vovó Benedita doces caseiros</title>
 </head>
 <body>
-    <div class="container-md">
-        <header>
-            <h1 class="text-center"><small>Vovó Benedita doces Caseiros</small></h1>
-            <nav class="nav" >
-                    <a class="nav-link" href="./index.php?p=produtos">Nossos Doces</a>
-                    <a class="nav-link" href="./index.php?p=quem_somos">Quem Somos</a>
-                    <a class="nav-link" href="./index.php?p=contato">Contato</a>
-                    <a class="nav-link" href="./index.php?p=localizacao">Localização</a>
-            
-            </nav>
-        </header>
-
-        <main>
-            
-            <?php
-                $valor = @$_GET['p'];
-                switch ($valor) {
-
-                    case "quem_somos": require_once './pages/quem_somos.php'; break;
-                    case "contato": require_once './pages/contato.php'; break;
-                    case "localizacao": require_once './pages/localizacao.php'; break;
-                    case "produtos": require_once './pages/produtos.php'; break;
-                    default: require_once './pages/produtos.php'; break;
-                }
-                    
-            ?>
-
-        </main>
-
-        <footer>
-            <p class="text-center">Rony Figueredo Corrêa RA 21158105-5</p>
-        </footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <header> 
+                    <nav>
+                        <ul class="nav justify-content-center">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="./index.php?p=produtos">Nossos Doces</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="./index.php?p=quem_somos">Quem Somos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="./index.php?p=contato">Contato</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="./index.php?p=localizacao">Localização</a>
+                            </li>
+                        </ul>               
+                    </nav>
+                </header>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <main>
+                <h1 class="text-center"><small>Vovó Benedita doces Caseiros</small></h1>
+                    <?php
+                        $valor = @$_GET['p'];
+                        switch ($valor) {
+                            case "quem_somos": require_once './pages/quem_somos.php'; break;
+                            case "contato": require_once './pages/contato.php'; break;
+                            case "localizacao": require_once './pages/localizacao.php'; break;
+                            case "produtos": require_once './pages/produtos.php'; break;
+                            default: require_once './pages/produtos.php'; break;
+                        }
+                    ?>
+                </main>
+            </div>
+        </div>       
+        <div class="row">
+            <div class="col">
+                <footer>
+                    <p class="text-center">Rony Figueredo Corrêa RA 21158105-5</p>
+                </footer>
+            </div>
+        </div>
         
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script> 
