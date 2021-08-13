b9e52aa099ffabb566ce15087efb99cf821233a0
ronydf <correa.rony@gmail.com>
2021-08-13 14:58:16 -0300

    sombra nas imagens

--- pages/doce.php
+++ pages/doce.php
@@ -1,6 +1,6 @@
 
 <h2 class="text-muted text-center"><?= $doces_descricao[$d]["title"]; ?></h2><br>
-<img src="<?=$produtos[$d]["foto"] ?>" class=" img-fluid rounded-circle rounded mx-auto d-block" alt="<?= $doces_descricao[$d]["title"]; ?>">
+<img src="<?=$produtos[$d]["foto"] ?>" class="shadow-lg p-1 mb-5 bg-body rounded img-fluid rounded-circle mx-auto d-block" alt="<?= $doces_descricao[$d]["title"]; ?>">
 <br><br>
 <p> <?= $doces_descricao[$d]["description"]; ?> </p>
     <ul>
--- pages/quem_somos.php
+++ pages/quem_somos.php
@@ -1,4 +1,6 @@
 <h2 class="text-muted">Quem Somos</h2>
+<br>
+<img src="./images/fabrica.jpg" class="shadow-lg p-1 mb-5 bg-body rounded img-fluid rounded-circle mx-auto d-block" alt="Nossa fábrica">
 <h5>Somos uma pequena industria instalada nas proximidade de Brasilia e trabalhos com produtos selecionados </h5>
 <p>Exercemos um papel social na região pois adiquirimos a produção de pequenos agricultores familiares propiciando renda para essas familias.</p>
 <p>Nossos produtos são inspecionados e certificados, garantindo a você doces com a mesma qualidade das grandes industrias<br>
