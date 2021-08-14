8f47341ab801aa4a8143899706b86f00f6c9d130
ronydf <correa.rony@gmail.com>
2021-08-13 21:39:17 -0300

    Update produtos.php

--- pages/produtos.php
+++ pages/produtos.php
@@ -1,4 +1,5 @@
 <div class="row" style="padding-top: 30px;">
+    <h2 class="text-muted">Nossos doces</h2>
     <?php for($i=0; $i<4; $i++) { ?> <!-- abre o laço de repetição para ler o array dos produtos disponíveis -->
         <!-- Monta os CARDs dos produtos -->
             <div class="col-3">
